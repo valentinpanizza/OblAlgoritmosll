4a050997671b92b32f8f7484855cc23fcf157222
valentinpanizza <valentinpanizza21@gmail.com>
2025-09-10 11:19:02 -0300

    Ej4: comentarios de como hacerlo. Ej2: implementacion inicial

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -5,14 +5,20 @@
 
 using namespace std;
 
-struct NodoHash {
-    string dominio;
+struct NodoLista {
     string path;
     string titulo;
     int tiempo;
-    bool activo;
-    NodoHash(string _dominio, string _path, string _titulo, int _tiempo) : dominio(_dominio), path(_path), titulo(_titulo), tiempo(_tiempo), activo(true) {}
-    NodoHash() : dominio(""), path(""), titulo(""), tiempo(0), activo(false) {}
+    NodoLista* sig;
+    NodoLista(string _path, string _titulo, int _tiempo) : path(_path), titulo(_titulo), tiempo(_tiempo), sig(NULL) {}
+};
+
+struct NodoHash {
+    string dominio;
+    NodoLista* l;
+    bool activo;    
+    NodoHash() : dominio(""), l(NULL), activo(false) {}
+    NodoHash(string _dominio, NodoLista* _l) : dominio(_dominio), l(_l), activo(true) {}
 };
 
 class HashCerrado{
@@ -21,18 +27,39 @@ class HashCerrado{
     int B;
     int N;
 
+    int obtenerPos(string _clave, int i){
+        int h1 = hash1(_clave);
+        int h2 = hash2(_clave);
+        int pos = normalizar(h1 + i * (h2 + 1));
+        if(!this->arr[pos].activo || this->arr[pos].dominio == _clave) return pos; // pos libre o mismo dom
+        return obtenerPos(_clave, i + 1);
+    }
+
+    void insertar(string _clave, NodoLista* el){
+        int pos = obtenerPos(_clave, 0);
+        if(arr[pos].activo){
+            el->sig = arr[pos].l;
+            arr[pos].l = el;
+        }else{
+            arr[pos].activo = true;
+            arr[pos].dominio = _clave;
+            arr[pos].l = el;
+            this->N++;
+        }
+    }
+
     int hash1(string key) {
-    int h = 0;
-    for (int i = 0; i < key.length(); i++)
-        h = 31 * h + int(key[i]);
-    return h;
+        int h = 0;
+        for (int i = 0; i < key.length(); i++)
+            h = 31 * h + int(key[i]);
+        return h;
     }
 
     int hash2(string key) {
-    int h = 0;
-    for (int i = 0; i < key.length(); i++)
-        h = h + key[i] * (i + 1);
-    return h;
+        int h = 0;
+        for (int i = 0; i < key.length(); i++)
+            h = h + key[i] * (i + 1);
+        return h;
     }
 
     int normalizar(int a){
@@ -44,6 +71,16 @@ class HashCerrado{
         B = capacidad;
         N = 0;
         arr = new NodoHash[B]();
+    }
+
+    void put(string dominio, string path, string titulo, int tiempo){
+        if(N == B) return;
+        NodoLista* nuevo = new NodoLista(path, titulo, tiempo);
+        insertar(dominio, nuevo);
+    }
+
+    float factorDeCarga() {
+        return (float)this->N/(float)this->B;
     }
 };
 
--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -4,7 +4,11 @@
 #include <limits>
 
 using namespace std;
-
+/*
+K-merge
+k listas ordenadas
+Grabacion mie 10 10:20 am
+*/
 int main()
 {
     // TODO
