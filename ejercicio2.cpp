64b89c6a17cad16450e178967671bced73b799b9
valentinpanizza <valentinpanizza21@gmail.com>
2025-09-10 22:44:14 -0300

    Ej2: terminado, no andan pruebas

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -27,25 +27,28 @@ class HashCerrado{
     int B;
     int N;
 
-    int obtenerPos(string _clave, int i){
+    int obtenerPos(string _clave) {
         int h1 = hash1(_clave);
         int h2 = hash2(_clave);
-        int pos = normalizar(h1 + i * (h2 + 1));
-        if(!this->arr[pos].activo || this->arr[pos].dominio == _clave) return pos; // pos libre o mismo dom
-        return obtenerPos(_clave, i + 1);
+        for (int i = 0; i < B; ++i) {
+            int pos = normalizar(h1 + i * h2);
+            if (!arr[pos].activo || arr[pos].dominio == _clave)
+                return pos;
+        }
+        return -1; // tabla llena
     }
 
-    void insertar(string _clave, NodoLista* el){
-        int pos = obtenerPos(_clave, 0);
-        if(arr[pos].activo){
-            el->sig = arr[pos].l;
-            arr[pos].l = el;
-        }else{
-            arr[pos].activo = true;
-            arr[pos].dominio = _clave;
-            arr[pos].l = el;
-            this->N++;
+    bool existe(string clave, string path){
+        int pos = obtenerPos(clave);
+        if(pos == -1) return false;
+        if(!arr[pos].activo) return false;
+        NodoLista* aux = arr[pos].l;
+        while(aux){
+            if(aux->path == path)
+                return true;
+            aux = aux->sig;
         }
+        return false;
     }
 
     int hash1(string key) {
@@ -66,6 +69,36 @@ class HashCerrado{
         return abs(a) % B;
     }
 
+    void insertarOrd(NodoLista*& lista, NodoLista* nuevo) {
+        if (!lista || nuevo->tiempo < lista->tiempo) {
+            nuevo->sig = lista;
+            lista = nuevo;
+        } else {
+            NodoLista* actual = lista;
+            while (actual->sig && actual->sig->tiempo < nuevo->tiempo) {
+                actual = actual->sig;
+            }
+            nuevo->sig = actual->sig;
+            actual->sig = nuevo;
+        }
+    }
+
+    NodoLista* sacarNodo(NodoLista*& raiz, string path) {
+        NodoLista* anterior = NULL;
+        NodoLista* aux  = raiz;
+        while (aux) {
+            if (aux->path == path) {
+                if (anterior) anterior->sig = aux->sig;
+                else      raiz = aux->sig;
+                aux->sig = NULL;
+                return aux;
+            }
+            anterior = aux;
+            aux  = aux->sig;
+        }
+        return NULL;
+    }
+
     public:
     HashCerrado(int capacidad){
         B = capacidad;
@@ -74,18 +107,167 @@ class HashCerrado{
     }
 
     void put(string dominio, string path, string titulo, int tiempo){
-        if(N == B) return;
+        int pos = obtenerPos(dominio);
+        if(pos == -1) return;
+        if(!arr[pos].activo){ 
+            arr[pos].dominio = dominio;
+            arr[pos].l = NULL;
+            arr[pos].activo = true;
+        }
+        NodoLista*& aux = arr[pos].l;
+        if(existe(dominio, path)){
+            NodoLista* nodo = sacarNodo(aux, path);
+            if(nodo){
+                nodo->titulo = titulo;
+                nodo->tiempo = tiempo;
+                insertarOrd(aux, nodo);
+                return;
+            }
+        }
         NodoLista* nuevo = new NodoLista(path, titulo, tiempo);
-        insertar(dominio, nuevo);
+        insertarOrd(aux, nuevo);
+        this->N++;
     }
 
     float factorDeCarga() {
         return (float)this->N/(float)this->B;
     }
+
+    string get(string dominio, string path){
+        int pos = obtenerPos(dominio);
+        if(pos == -1 || !arr[pos].activo) return "recurso_no_encontrado";
+        NodoLista* aux = arr[pos].l;
+        while(aux){
+            if(aux->path == path)
+                return aux->titulo + " " + to_string(aux->tiempo);
+            aux = aux->sig;
+        }
+        return "recurso_no_encontrado";
+    }
+
+    string contains(string dominio, string path) {
+        return existe(dominio, path) ? "true" : "false";
+    }
+
+    void clear_domain(string dominio) {
+        int pos = obtenerPos(dominio);
+        if(pos == -1 || !arr[pos].activo) return;
+        NodoLista* aux = arr[pos].l;
+        while(aux){
+            NodoLista* borro = aux;
+            aux = aux->sig;
+            delete borro;
+            this->N--;
+        }
+        arr[pos].l = NULL;
+        arr[pos].activo = false;
+        arr[pos].dominio = "";
+    }
+
+    int size() {
+        return N;
+    }
+
+    void clear() {
+        for(int i = 0; i < B; ++i){
+            NodoLista* aux = arr[i].l;
+            while(aux){
+                NodoLista* borro = aux;
+                aux = aux->sig;
+                delete borro;
+            }
+            arr[i].l = NULL;
+            arr[i].activo = false;
+            arr[i].dominio = "";
+        }
+        N = 0;
+    }
+
+    void remove(string dominio, string path) {
+        int pos = obtenerPos(dominio);
+        if(pos == -1 || !arr[pos].activo) return;
+        NodoLista* aux = arr[pos].l;
+        NodoLista* prev = NULL;
+        while(aux){
+            if(aux->path == path){
+                if(prev) prev->sig = aux->sig;
+                else arr[pos].l = aux->sig;
+                delete aux;
+                this->N--;
+                return;
+            }
+            prev = aux;
+            aux = aux->sig;
+        }
+    }
+
+    int count_domain(string dominio){
+        int pos = obtenerPos(dominio);
+        if(pos == -1 || !arr[pos].activo) return 0;
+        int count = 0;
+        NodoLista* aux = arr[pos].l;
+        while(aux){
+            count++;
+            aux = aux->sig;
+        }
+        return count;
+    }
+
+    string list_domain(string dominio){
+        int pos = obtenerPos(dominio);
+        if(pos == -1 || !arr[pos].activo) return "";
+        string ret = "";
+        NodoLista* aux = arr[pos].l;
+        while(aux){
+            ret += aux->path ;
+            aux = aux->sig;
+            if(aux) ret += " ";
+        }
+        return ret;
+    }
 };
 
 int main()
 {
-    // TODO
-    return 0;
+    int cant;
+    std::cin >> cant;
+    HashCerrado* tabla = new HashCerrado(cant);
+    for (int i = 0; i < cant; ++i) {
+        string op;
+        cin >> op;
+        if (op == "PUT") {
+            int tiempo;
+            string dominio, path, titulo;
+            cin >> dominio >> path >> titulo >> tiempo;
+            tabla->put(dominio, path, titulo, tiempo);
+        } else if (op == "GET") {
+            string dominio, path;
+            cin >> dominio >> path;
+            cout << tabla->get(dominio, path) << endl;
+        } else if (op == "REMOVE") {
+            string dominio, path;
+            cin >> dominio >> path;
+            tabla->remove(dominio, path);
+        } else if (op == "CONTAINS") {
+            string dominio, path;
+            cin >> dominio >> path;
+            cout << tabla->contains(dominio, path) << endl;
+        } else if (op == "COUNT_DOMAIN") {
+            string dominio;
+            cin >> dominio;
+            cout << tabla->count_domain(dominio) << endl;
+        } else if (op == "LIST_DOMAIN"){
+            string dominio;
+            cin >> dominio;
+            cout << tabla->list_domain(dominio) << endl;
+        } else if (op == "CLEAR_DOMAIN"){
+            string dominio;
+            cin >> dominio;
+            tabla->clear_domain(dominio);
+        } else if (op == "SIZE"){
+            cout << tabla->size() << endl;
+        } else if (op == "CLEAR"){
+            tabla->clear();
+        }
+    }
 }
